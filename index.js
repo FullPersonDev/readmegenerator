e36c7128e0164cd50c6120b62cd9d2fecd0c9137
Martin Aguero <martin.aguero84@gmail.com>
2024-01-09 10:41:51 -0600

    put together the questions object with the necessary questions to build the readme, tested that it work in the console.

--- index.js
+++ index.js
@@ -9,22 +9,49 @@ const questions = [
         message: 'What should be the title for your README file?'
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a short description for your README file:'
-    },
-    {
         type: 'list',
         name: 'table',
         message: 'Do you want a table of content?',
         choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description for your application:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your application:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide testing instructions:'
     }
 ];
 inquirer.prompt(questions).then(answers => {
-    console.log(`Here is what you said.
-    Your title is: ${answers.title}.
-    Your description is: ${answers.description}.
-    If you want table of content: ${answers.table}.`);
+    console.log(
+`Here is what you said.
+    Your title is: ${answers.title}
+    If you want table of content: ${answers.table}
+    Your description is: ${answers.description}
+    Your installation is: ${answers.installation}
+    Your usage is: ${answers.usage}
+    Your contribution is: ${answers.contribution}
+    Your test is: ${answers.test}
+    
+Thank you`);
 });
 
 // TODO: Create a function to write README file
